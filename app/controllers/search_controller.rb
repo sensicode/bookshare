6c599aa759dd27f952f7861fa0ddafc50d29a019
Adrian Short <adrian.short@gmail.com>
2011-01-12 15:34:01 +0000

    Fixing search SQL query for Heroku postgres

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,11 @@ class SearchController < ApplicationController
     @q = params[:q].strip
 #     @type = params[:type]
 #     if @type == 'titles'
+      # sqlite uses || operator to concat strings, postgres uses TEXTCAT
       @titles = Title.find_by_sql("
         SELECT *
         FROM titles
-        WHERE title || ' ' || subtitle LIKE('%#{params[:q]}%')
+        WHERE TEXTCAT(TEXTCAT(title, ' '), subtitle) LIKE('%#{params[:q]}%')
       " )
 #     elsif @type == 'authors'
 #       @titles = Author.find_by_sql("
