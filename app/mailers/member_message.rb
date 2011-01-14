e397915b8a7e9ebdd2ce25e4ae88435d35bfa2f8
Adrian Short <adrian.short@gmail.com>
2011-01-14 16:40:30 +0000

    Added config for production mailer

--- app/mailers/member_message.rb
+++ app/mailers/member_message.rb
@@ -1,6 +1,5 @@
 class MemberMessage < ActionMailer::Base
   default :from => "adrian.short@gmail.com"
-#   default_url_options[:host] = "www.suttonbookshare.org.uk"
   
   def member_message(sender, recipient, message)
     @message = message
