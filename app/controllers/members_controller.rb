72e0e900cc49952d81c484a816b8936420309138
Adrian Short <adrian.short@gmail.com>
2011-02-28 16:47:20 +0000

    Added view for /members with 30 random members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,8 @@
 class MembersController < ApplicationController
+  def index
+    @members = User.all :order => 'RANDOM()', :limit => 30
+  end
+
   def show
     @user = User.find_by_login(params[:login])
     @very_overdue_count = 0
