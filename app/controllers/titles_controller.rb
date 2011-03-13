712437b5d07c2bc227f6d64f94c38d2c2333504a
Adrian Short <adrian.short@gmail.com>
2011-03-13 14:38:48 +0000

    Removed redundant create method; temporarily removed XML and JSON representations

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -1,30 +1,15 @@
 class TitlesController < ApplicationController
-  # POST /titles
-  # POST /titles.xml
-  def create
-    @title = Title.create_from_isbn13(params[:isbn13])
-
-    respond_to do |format|
-      if @title.save
-        format.html { redirect_to(@title, :notice => 'Title was successfully created.') }
-        format.xml  { render :xml => @title, :status => :created, :location => @title }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @title.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-  
   # GET /titles/1
   # GET /titles/1.xml
+  # GET /titles/1.json
   def show
     @title = Title.find(params[:id])
     @watching = Watching.new
     
     respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @title }
-      format.json { render :json => @title }
+      format.html
+#       format.xml  { render :xml => @title }
+#       format.json { render :json => @title }
     end
   end
 end
