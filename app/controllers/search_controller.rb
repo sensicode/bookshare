f74707a9f560a25e3588cd5a806e65bbb07b82ec
Adrian Short <adrian.short@gmail.com>
2011-07-21 17:37:13 +0100

    Revert "Quick SQLITE3 version of the search sql"
    This reverts commit 450336d323f23e5f354321a7e08bceba7792be61.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,24 +1,14 @@
 class SearchController < ApplicationController
   def index
     @q = params[:q].strip.downcase
-
-
-    case ActiveRecord::Base.connection.adapter_name
-    when 'SQLite'
-      @titles = Title.find_by_sql(["
-        SELECT *
-        FROM titles
-        WHERE title LIKE ? OR subtitle LIKE ?", "%#{@q}%", "%#{@q}%"])
-    else
+#     @type = params[:type]
+#     if @type == 'titles'
+      # sqlite uses || operator to concat strings, postgres uses TEXTCAT
       @titles = Title.find_by_sql(["
         SELECT *
         FROM titles
         WHERE LOWER(TEXTCAT(TEXTCAT(title, ' '), subtitle)) LIKE(?)", "%#{@q}%"
       ])
-    end
-#     @type = params[:type]
-#     if @type == 'titles'
-      # sqlite uses || operator to concat strings, postgres uses TEXTCAT
       Search.create(:query => @q, :results => @titles.count)
 
 #     elsif @type == 'authors'
