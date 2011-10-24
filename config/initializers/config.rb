55f4f380f87566c09f27ca2151cb191c674da883
Adrian Short <adrian.short@gmail.com>
2011-10-24 16:56:23 +0100

    Typo

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -2,7 +2,7 @@ CONFIG = {
   :sitename =>           ENV['BOOKSHARE_SITENAME'],
   :theme =>              ENV['BOOKSHARE_THEME'],
   :email =>              ENV['BOOKSHARE_EMAIL'],
-  :no_reply_email =>     ENV['BOOKSHARE_NO_REPLY_EMAIIL'],
+  :no_reply_email =>     ENV['BOOKSHARE_NO_REPLY_EMAIL'],
   :twitter =>            ENV['BOOKSHARE_TWITTER'],
   :google_analytics =>   ENV['BOOKSHARE_GOOGLE_ANALYTICS']
 }
